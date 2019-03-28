8099f5f54e99286f3df57352de36404c5618b462
Cleiton Melo <cleiton.melo@gmail.com>
2019-03-28 17:26:58 -0300

    Criando Entidades e Repository de Estado e Cidade

---
+++ src/main/java/com/cleitonmelo/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.cleitonmelo.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cleitonmelo.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+	
+}
