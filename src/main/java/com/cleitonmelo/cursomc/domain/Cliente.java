14d2f5a7bfcd83043ad930f145d04b1e04240c52
Cleiton Melo <cleiton.melo@gmail.com>
2019-04-04 17:23:12 -0300

    EndPoint /pedidos/{id} disponibilizado

--- src/main/java/com/cleitonmelo/cursomc/domain/Cliente.java
+++ src/main/java/com/cleitonmelo/cursomc/domain/Cliente.java
@@ -15,6 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.cleitonmelo.cursomc.domain.enums.TipoCliente;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
@@ -37,6 +38,7 @@ public class Cliente implements Serializable {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	@JsonBackReference
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
@@ -52,7 +54,6 @@ public class Cliente implements Serializable {
 		this.cpfOuCnpj = cpfOuCnpj;
 		this.tipo = tipo.getCod();
 	}
-
 	
 	@Override
 	public int hashCode() {
